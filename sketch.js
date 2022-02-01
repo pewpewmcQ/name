167f5a8a351f74a08cd13f88594679a22b0c9405
pewpewmcQ <93830526+pewpewmcQ@users.noreply.github.com>
2022-02-01 04:52:14 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//var age = 12
+//var name = "Dietrich"
+//var grade = 8
+//var birthday = "03/22/08"
+var s1;
+
+
+function setup() {
+  createCanvas(400,400);
+ s1 = new Student("dietrich",13,8,"03/22/08");
+ s1.display();
+
+  //console.log(name);
+ // console.log(age);
+  //console.log(birthday);
+ // console.log(grade);
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,19 @@
+
+class Student
+{
+   constructor(name,age,grade,birthday){
+    this.name = name;
+    this.grade = grade;
+    this.birthday = birthday;
+    this.age = age;
+   }
+   display(){
+       console.log(this.name);
+       console.log(this.grade);
+       console.log(this.age);
+       console.log(this.birthday);
+   }
+}
+
+
+
